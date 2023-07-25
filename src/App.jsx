30fea6e1866f7249cb7ec98478b8513e356c1ed9
sophie Haasnoot <ms.haasnoot@gmail.com>
2023-07-25 21:03:41 +0200

    Html structure of Weather component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
         <Weather defaultCity="Amsterdam"/>
 
         <footer>
--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,5 +1,42 @@
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Hello from Weather</div>
+  return <div className="Weather">
+    <form>
+      <div className="row">
+        <div className="col-9">
+          <input
+            type="search"
+            placeholder="Enter a city..."
+            className="form-control"/>
+        </div>
+        <div className="col-3">
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary"/>
+        </div>
+      </div>
+
+    </form>
+    <h1>Amsterdam</h1>
+    <ul>
+      <li>Wednesday 7:00</li>
+      <li>Mostly Cloudy</li>
+    </ul>
+    <div className="row">
+      <div className="col-6">
+        <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="partly-cloudy" />
+        6 °C
+      </div>
+      <div className="col-6">
+        <ul>
+          <li>Precipitation: 15%</li>
+          <li>Humidity: 72%</li>
+          <li>Wind: 20 km/h</li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
 }

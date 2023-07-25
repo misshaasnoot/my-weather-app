6391d5fb2b99da9d5f41464aa559ed8f0eaa37f9
sophie Haasnoot <ms.haasnoot@gmail.com>
2023-07-25 20:34:19 +0200

    Add Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import "./App.css";
+import 
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by{" "}
-        <a href="https://sophiehaasnoot.netlify.app"
-        target="_blank"
-        rel="noreferrer">Sophie Haasnoot</a>{" "}and is{" "}
-        <a href="https://github.com/misshaasnoot/my-weather-app"
-        target="_blank"
-        rel="noreferrer">open sourced</a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by{" "}
+          <a href="https://sophiehaasnoot.netlify.app"
+          target="_blank"
+          rel="noreferrer">
+            Sophie Haasnoot
+            </a>{" "}and is{" "}
+          <a href="https://github.com/misshaasnoot/my-weather-app"
+          target="_blank"
+          rel="noreferrer">open sourced</a>
+        </footer>
+      </div>
     </div>
+
 
   )
 }

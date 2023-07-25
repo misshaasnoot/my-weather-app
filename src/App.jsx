c45b19ac19dcc19f457d8df57b1024461ccc5a9a
sophie Haasnoot <ms.haasnoot@gmail.com>
2023-07-25 17:06:06 +0200

    Added Git repository and portfolio in footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,19 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by{" "}
+        <a href="https://sophiehaasnoot.netlify.app"
+        target="_blank"
+        rel="noreferrer">Sophie Haasnoot</a>{" "}and is{" "}
+        <a href="https://github.com/misshaasnoot/my-weather-app"
+        target="_blank"
+        rel="noreferrer">open sourced</a>
+      </footer>
+    </div>
+
   )
 }
 

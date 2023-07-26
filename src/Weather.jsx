3f0d2be95a363251c42f84b5c4d01aa25199ebe0
sophie Haasnoot <ms.haasnoot@gmail.com>
2023-07-26 10:42:13 +0200

    Api integration

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
+import PropTypes from 'prop-types';
 
 
 
@@ -35,38 +36,42 @@ export default function Weather(props) {
     let apiKey = "dff5c692192605ee5ed7f95b423ae857";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
-}
+  }
 
 
-if (weatherData.ready) {
-  return (
-    <div className="Weather">
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              className="form-control"
-              autoFocus="on"
-              onChange={handleCityChange}
-            />
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <WeatherInfo data={weatherData} />
-      <WeatherForecast coordinates={weatherData.coordinates} />
-    </div>
-  );
-} else {
-  search();
-  return "Loading...";
+        </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
+
+Weather.propTypes = {
+  defaultCity: PropTypes.string.isRequired
 }
